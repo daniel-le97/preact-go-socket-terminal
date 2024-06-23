0f0bfea9c389032936bf1ad3f4134e9232bc842c
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-06-23 11:41:07 -0600

    work on adding sqlite

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import TerminalComponent from './terminal'
 import {spawn} from 'socket:child_process'
 
+// TODO - this is a hack to get the terminal to render, should render app but connect websocket once the binary is spawned
 
 const terminalBinary = spawn('./binaries/terminal')
 terminalBinary.stderr.on('data', (data:Buffer) => {
